fb76bc36dcff2222dcf3267bec6c941b017bc931
Charles McKelvey <chazmckelvey@gmail.com>
2019-10-03 15:41:40 -0400

    Troubleshooting datastructure

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function main() {
+	// This is for the login process
 	const login = document.querySelector('#login');
 	document.querySelector('#login').addEventListener('submit', (event) => {
 		event.preventDefault();
@@ -11,13 +12,57 @@ function main() {
 		const username = loginData.get('username');
 		const password = loginData.get('password');
 		console.log({ loginData }, { username }, { password });
-		// Fetching naturally GETs information out, so we have to set the POST.
-		fetch('https://notes-api.glitch.me/api/users', {
-			method: POST
-		})
-			.then((response) => response.json())
-			.then((data) => {});
+
+		fetch('https://notes-api.glitch.me/api/notes', {
+			method: 'GET'
+		});
 	});
 }
+
+const app = {
+	data: {
+		credientials: {
+			username: sessionStorage.getItem('username'),
+			password: sessionStorage.getItem('password')
+		},
+		notes: []
+	}
+};
+
+class App {
+	constructor(user) {
+		this.user = user;
+	}
+	createNote() {}
+
+	getAllNotes() {}
+}
+
+class User {
+	constructor(username, password) {
+		this.username = username;
+		this.password = password;
+		this.auth = 'Basic ' + btoa(username + ':' + password);
+	}
+
+	getUsername() {
+		return this.username;
+	}
+
+	getPassword() {
+		return this.password;
+	}
+
+	getAuth() {
+		return this.auth;
+	}
+}
+
+fetch('https://notes-api.glitch.me/api/notes', {
+	method: 'GET',
+	header: {
+		Authorization: 'Basic ' + btoa(app.data.credientials.username + ':' + app.data.credientials.password)
+	}
+});
 
 main();
