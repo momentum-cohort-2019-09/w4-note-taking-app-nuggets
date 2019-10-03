214ace1e1f97a107a718fe456ce12fd46172f823
Charles McKelvey <chazmckelvey@gmail.com>
2019-10-03 16:48:14 -0400

    Adding some styling

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function main() {
 	const login = document.querySelector('#login');
 	document.querySelector('#login').addEventListener('submit', (event) => {
 		event.preventDefault();
+
 		// Must initially grab the form
 		const loginData = new FormData(login);
 		// We are getting these via their -name- attributes.
@@ -12,10 +13,8 @@ function main() {
 		const username = loginData.get('username');
 		const password = loginData.get('password');
 		console.log({ loginData }, { username }, { password });
-
-		fetch('https://notes-api.glitch.me/api/notes', {
-			method: 'GET'
-		});
+		app.login(username, password);
+		app.getAllNotes();
 	});
 }
 
@@ -26,43 +25,77 @@ const app = {
 			password: sessionStorage.getItem('password')
 		},
 		notes: []
+	},
+
+	getAuthHeader() {
+		// This will auto populate the header for when we need authorization
+		// Easiability is nioce
+	},
+
+	setCredientials: function(username, password) {
+		this.data.credientials = {
+			username: username,
+			password: password
+		};
+		sessionStorage.setItem('username', username);
+		sessionStorage.setItem('password', password);
+	},
+
+	login: function(username, password) {
+		fetch('https://notes-api.glitch.me/api/users', {
+			headers: {
+				Authorization: 'Basic ' + btoa(`${username}:${password}`)
+			}
+		}).then((response) => {
+			if (response.ok) {
+				this.setCredientials(username, password);
+				document.querySelector('#login').classList.remove('input-invalid');
+				// Need a way to then render the notes for this given user,
+				// because the login was successful.
+			} else {
+				document.querySelector('#login').classList.add('input-invalid');
+				document.querySelector('#error').innerText = 'That is not a valid username and password.';
+			}
+		});
+	},
+
+	getAllNotes: function() {
+		fetch('https://notes-api.glitch.me/api/notes', {
+			headers: {
+				Authoritization:
+					'Basic ' + btoa(`${this.data.credientials.username}:${this.data.credientials.password}`)
+			}
+		})
+			.then((response) => {
+				if (response.status === 401) {
+					// What if yknow
+				} else {
+					return response.json();
+				}
+			})
+			.then((response) => {
+				if (response.ok) {
+					document.getElementByID('note').innerHTML = data.map(generateNoteHTML).join('/n');
+				}
+			})
+			.catch((error) => {});
+	},
+
+	updateNote: function(note /* Right? */) {
+		// Needs to be able to take the given note and update title, text, and tags
+		// Using PUT with https://notes-api.glitch.me/api/notes/:id
+	},
+
+	deleteNote: function(note) {
+		// Needs to be able to DELETE a given note from the users notes array
+		// Then also DELETE the note from the API as well.
+		// Using DELETE with https://notes-api.glitch.me/api/notes/:id
+	},
+
+	getTaggedNotes: function(tag) {
+		// Needs to be able to get the tags given the clicked tag. Probably an eventListener? (For sure a later idea)
+		// Using GET with https://notes-api.glitch.me/api/notes/tagged/:tag
 	}
 };
-
-class App {
-	constructor(user) {
-		this.user = user;
-	}
-	createNote() {}
-
-	getAllNotes() {}
-}
-
-class User {
-	constructor(username, password) {
-		this.username = username;
-		this.password = password;
-		this.auth = 'Basic ' + btoa(username + ':' + password);
-	}
-
-	getUsername() {
-		return this.username;
-	}
-
-	getPassword() {
-		return this.password;
-	}
-
-	getAuth() {
-		return this.auth;
-	}
-}
-
-fetch('https://notes-api.glitch.me/api/notes', {
-	method: 'GET',
-	header: {
-		Authorization: 'Basic ' + btoa(app.data.credientials.username + ':' + app.data.credientials.password)
-	}
-});
 
 main();
