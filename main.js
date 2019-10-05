31c46ae58b209dcfe519c695473f2a428afb4cb2
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-05 16:49:24 -0400

    notes now is created after login, but still getting 422

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const app = {
 		notes: []
 	},
 	setAuthHeader: function (headers) {
-		// This will auto populate the header for when we need authorization
-		// Easiability is nioce
 		if (!headers) {
 			header = {}
 		}
@@ -17,7 +15,6 @@ const app = {
 	},
 
 	setCredentials: function (username, password) {
-		this.data = {};
 		this.data.credentials = {
 			username: username,
 			password: password
@@ -35,10 +32,10 @@ const app = {
 			if (response.ok) {
 				this.setCredentials(username, password);
 				document.querySelector('#login').classList.remove('input-invalid');
-				// Append all notes to the this.data.notes array
 
-				// Need a way to then render the notes for this given user,
-				// because the login was successful.
+				document.querySelector('#login').classList.add('hidden')
+				document.querySelector('#createNote').classList.remove('hidden')
+	
 			} else {
 				document.querySelector('#login').classList.add('input-invalid');
 				document.querySelector('#error').innerText = 'That is not a valid username and password.';
@@ -64,25 +61,7 @@ const app = {
 	},
 
 	render: function () {
-		let createNote = document.querySelector('#create-note')
-		if (!createNote) {
-			document.querySelector('#replacement').innerHTML = `
-			<div id='create-note'>
-				<div class='input-field'>
-					<label for='title'>Title:</label>
-					<input id='title-text' type='text' name="title" placeholder="Put title here">
-				</div>
-				<div class='input-field'>
-					<label for='text'>Text:</label>
-					<textarea id='texty-text' type='text' name="text-area" placeholder="Put description here"></textarea>
-				</div>
-				<div class='input-field'>
-					<label for='tag'>Tag:</label>
-					<input type="text" name="tag" placeholder="Separate with comma and space">
-				</div>
-				<button id="submitNote" type='submit' value='submit'>Submit Note</button>
-			</div>`
-		}
+
 		let templateLiteral = ``
 		console.log({ app })
 		// Grab the notes and then loop through them 
@@ -117,24 +96,23 @@ const app = {
 		}
 		// Adding and also checking if the createNote area is made
 
-		let submitNote = document.querySelector('#submitNote')
-		submitNote.addEventListener('click', createNote())
-
 	},
 
-	createNote: function () {
+	createNote: function (title, text, tags) {
+		let tagsSplit = tags.split(',').map(tag => tag.trim())
+		console.log({tagsSplit})
 		fetch("https://notes-api.glitch.me/api/notes", {
 			method: 'POST',
+			body: JSON.stringify({`title`: title, `text`: text, `tags`: tagsSplit}),
 			headers: {
-				'Authorization': this.setAuthHeader()
+				'Authorization': this.setAuthHeader(),
+				'Content-Type': 'application/json'
 			}
 		})
 			.then(response => {
 				if (response.ok) {
-					let title = document.querySelector('#title-text').innerText
-					let textyText = document.querySelector('#texty-text').innerText
-					let tags = document.querySelector('#')
-				}
+					app.getAllNotes()
+				} 
 			})
 	},
 
@@ -168,25 +146,28 @@ const app = {
 };
 
 function main() {
-	// This is for the login process
+	
 	const login = document.querySelector('#login');
 	document.querySelector('#login').addEventListener('submit', (event) => {
 		event.preventDefault();
-
-		// Must initially grab the form
 		const loginData = new FormData(login);
-		// We are getting these via their -name- attributes.
-		// Throw some console logs below for clarification.
-		// Not sure if it being called loginData is confusing,
-		// but we can set it back to formData if that is more readable.
+
 		const username = loginData.get('username');
 		const password = loginData.get('password');
 		console.log({ loginData }, { username }, { password });
 		app.login(username, password);
 		app.getAllNotes();
-		// How do we grab a specific tag that is clicked?
+
 	});
 
+	let submitNote = document.querySelector('#submitNote')
+	submitNote.addEventListener('click', event => {
+		event.preventDefault()
+		let title = document.querySelector('#titleText').innerText
+		let textyText = document.querySelector('#textyText').innerText
+		let tags = document.querySelector('#tags').innerText
+		app.createNote(title, textyText, tags)
+	})
 }
 
 main();
